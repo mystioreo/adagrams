c864441f5958e4042631dd5c94c3b1066c2a86eb
Daniela Di Iorio <danielad@gmail.com>
2018-08-15 14:46:26 -0700

    corrected our uses available letters method and dictionary support

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -2,18 +2,24 @@ require 'csv'
 
 
 def draw_letters
-  all_letters = %w[A A A A A A A A A B B C C D D D D E E E E E E E E E E E E F F G G G H H I I I I I I I I I J K L L L L M M N N N N N N \
-    O O O O O O O O P P Q R R R R R R S S S S T T T T T T U U U U V V W W X Y Y Z ]
+  all_letters = %w[A A A A A A A A A B B C C D D D D E E E E E E E E E E E E F F
+    G G G H H I I I I I I I I I J K L L L L M M N N N N N N O O O O O O O O P P
+    Q R R R R R R S S S S T T T T T T U U U U V V W W X Y Y Z ]
+
   return (all_letters.shuffle).first(10)
 end
+
 
 def uses_available_letters?(input,letters_in_hand)
   value = true
   input_letters = (input.upcase).split('')
+  hand_copy = letters_in_hand.map do |letter|
+    letter
+  end
 
   input_letters.each do |alphabet|
-    if letters_in_hand.include?(alphabet)
-      letters_in_hand.delete_at(letters_in_hand.index(alphabet))
+    if hand_copy.include?(alphabet)
+      hand_copy.delete_at(hand_copy.index(alphabet))
     else
       value = false
     end
@@ -69,11 +75,10 @@ end
 
 def is_in_english_dict?(input)
 
-  require 'csv'
-
   truth_value = false
-  CSV.foreach("assets/dictionary-english.csv") do |row|
-    if input == row
+
+  CSV.foreach('assets/dictionary-english.csv') do |row|
+    if input == row[0]
       truth_value = true
       return truth_value
     end
